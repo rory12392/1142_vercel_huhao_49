e6fb6a42f2c0132a501d998e33011da57fcc4ef5
rory12392 <rory9256@gmail.com>
2026-04-22 21:10:03 +0800

    add Mid_49

--- src/components/Navbar_49.tsx
+++ src/components/Navbar_49.tsx
@@ -4,6 +4,7 @@ const Navbar_49 = () => {
   return (
     <nav className='max-w-3xl flex gap-x-4 mx-auto py-4'>
       <Link href='/'>Home</Link>
+      <Link href='/mid_49'>Mid_49</Link>
       <Link href='/counter_49'>Counter_49</Link>
       <Link href='/tours_49'>Tours_49</Link>
       <Link href='/actions_49'>Actions_49</Link>
